5aa9d5e01480fb6137dc581b3b87aa75df7910f9
Alpine Devcamp <alpine@devcamp.com>
2017-06-14 17:07:23 +0000

    Cleared git cache

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -29,10 +29,10 @@ class BlogsController < ApplicationController
     respond_to do |format|
       if @blog.save
         format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
-        format.json { render :show, status: :created, location: @blog }
+        
       else
         format.html { render :new }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
+        
       end
     end
   end
@@ -56,7 +56,7 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.html { redirect_to blogs_url, notice: 'Post was removed.' }
       format.json { head :no_content }
     end
   end
