388eb4ac7bed4f5fb901ed1d303ff3544b996421
Alpine Devcamp <alpine@devcamp.com>
2017-06-19 17:42:42 +0000

    Implemented show functionality for portfolio module

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -29,14 +29,17 @@ class BlogsController < ApplicationController
     respond_to do |format|
       if @blog.save
         format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
-        
       else
         format.html { render :new }
-        
       end
     end
   end
 
+def show
+  @portfolio_item = Portfolio.find(params[:id])
+end
+
+end
   # PATCH/PUT /blogs/1
   # PATCH/PUT /blogs/1.json
   def update
