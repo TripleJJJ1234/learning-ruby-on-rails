26a61b4447245a5eb2626c2d6cc435d1a7d3d674
Alpine Devcamp <alpine@devcamp.com>
2017-07-20 16:35:09 +0000

    Integrated cocoon for portfolio items nested form component

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,64 +4,55 @@ class PortfoliosController < ApplicationController
     access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
     
    def index
-   @portfolio_items = Portfolio.by_position
+     @portfolio_items = Portfolio.by_position
    end
    
    def sort
-       params[:order].each do |key, value|
-         Portfolio.find(value[:id]).update(position: value[:position])
+     params[:order].each do |key, value|
+       Portfolio.find(value[:id]).update(position: value[:position])
    end
   
      render nothing: true
    end
+   
    def angular
-        @angular_portfolio_items = Portfolio.angular
+     @angular_portfolio_items = Portfolio.angular
    end
    
    def new
-       @portfolio_item = Portfolio.new
-       3.times { @porfolio_item.technologies.build }
+     @portfolio_item = Portfolio.new
    end
    
-    def create
-    @portfolio_item = Blog.new(params.require(:portfolio).permit(:title, :subtitle, :body, 
-    technologies_attributes: [:name]))
+   def create
+     @portfolio_item = Portfolio.new(portfolio_params)
 
-    respond_to do |format|
-      if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
-      else
-        format.html { render :new }
-        
+     respond_to do |format|
+        if @portfolio_item.save
+          format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+        else
+          format.html { render :new }
+        end
       end
-    end
+   end
+
+  def edit
   end
 
-def edit
-    @portfolio_item = Portfolio.find(params [:id])
-end
-
  def update
-     @portfolio_item = Portfolio.find(params[:id])
-     
     respond_to do |format|
-      if @blog.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
       end
     end
-  end
+ end
   
   def show
-      @porfolio_item = Portfolio.find(params[:id])
   end
   
   def destroy
-      # Perform the lookup
-       @porfolio_item = Portfolio.find(params[:id])
-       
-       # Destroy/delete the record
+    # Destroy/delete the record
     @porfolio_item.destroy
     
     # Redirect
@@ -70,5 +61,19 @@ end
     end
   end
  
- end
+  private
   
+  def portfolio_params
+      params.require(:portfolio).permit(:title,
+                                        :subtitle,
+                                        :body,
+                                        :main_image,
+                                        :thumb_image,
+                                        technologies_attributes: [:id, :name, :_destroy]
+                                        )
+  end
+  
+  def set_portfolio_item
+      @portfolio_item = Portfolio.find(params[:id])
+  end
+end
